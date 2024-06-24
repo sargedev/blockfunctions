3c4f0d5850b9703e3def97da9722365d7c917924
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-24 14:06:06 +0200

    Created BlockFunction class

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
 
+namespace blockFunctions {
+    class BlockFunction {
+        method: (args: any[]) => any;
+        args: string[];
+        values: any[];
+
+        constructor(method: (args: any[]) => any, args: string[]) {
+            this.method = method;
+            this.args = args;
+        }
+
+        call(values: any[]) {
+            this.method(this.values);
+        }
+    }
+}
